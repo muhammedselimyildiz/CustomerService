be69f3f55cc9485407e3ae340f42376fa11f3c24
M. Selim Yildiz <muhammed.yildiz11@ogr.sakarya.edu.tr>
2022-12-26 01:41:53 +0300

    Post and Put Mapping added

--- src/main/java/com/selimyildiz/CustomerService/controller/CustomerController.java
+++ src/main/java/com/selimyildiz/CustomerService/controller/CustomerController.java
@@ -1,12 +1,12 @@
 package com.selimyildiz.CustomerService.controller;
 
 import com.selimyildiz.CustomerService.model.Customer;
+import org.springframework.boot.web.server.WebServerException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
@@ -16,14 +16,54 @@ import static org.springframework.http.HttpStatus.OK;
 @RequestMapping("/customers")
 public class CustomerController
 {
-    @GetMapping
-    public ResponseEntity<List<Customer>> getCustomer()
+    private static final List<Customer> customers = new ArrayList<>();
+
+    public CustomerController()
     {
-        Customer customer1 = new Customer(1, "Selim", "Yildiz");
-        Customer customer2 = new Customer(2, "Murat", "Yildiz");
+        if (customers.isEmpty())
+        {
+            Customer customer1 = new Customer(1, "Selim", "Yildiz");
+            Customer customer2 = new Customer(2, "Murat", "Yilmaz");
 
-        List<Customer> customers = Arrays.asList(customer1, customer2);
+            customers.add(customer1);
+            customers.add(customer2);
+        }
 
+
+
+    }
+    @GetMapping
+    public ResponseEntity<List<Customer>> getCustomers()
+    {
         return new ResponseEntity<>(customers, OK);
+    }
+
+    @GetMapping("/{customerId}")
+    public ResponseEntity<Customer> getCustomer(@PathVariable int customerId)
+    {
+        Customer result = getCustomerById(customerId);
+        return new ResponseEntity<>(result, OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Customer> createCustomer(@RequestBody Customer newCustomer)
+    {
+        customers.add(newCustomer);
+        return new ResponseEntity<>(newCustomer, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{customerId}")
+    public ResponseEntity<Void> getCustomer(@PathVariable int customerId, @RequestBody Customer newCustomer)
+    {
+        Customer oldCustomer = getCustomerById(customerId);
+        oldCustomer.setName(newCustomer.getName());
+        oldCustomer.setLastName(newCustomer.getLastName());
+
+        return new ResponseEntity<>(OK);
+    }
+    private static Customer getCustomerById(int customerId)
+    {
+        return customers.stream().filter(customer -> customer.getId() == customerId)
+                .findFirst().orElseThrow(() -> new RuntimeException("Customer not found."));
     }
 }
